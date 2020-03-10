7b17f9d28e67328711125e2d9c0313a6b113e0d1
Edwin <edwin@MacBook-Pro-Edwin.local>
2020-03-11 01:27:20 +0800

    test al

---
+++ algorithm/src/interview/ListInterAL.java
@@ -0,0 +1,47 @@
+package interview;
+
+import java.util.ArrayList;
+
+public class ListInterAL {
+
+    /**
+     * 遍历
+     * @param listNode
+     * @return
+     */
+    public ArrayList<Integer> printListFromTailToHead(LinkedListAL.ListNode listNode) {
+
+    }
+
+    /**
+     * 结点相关
+     */
+    public LinkedListAL.ListNode EntryNodeOfLoop(LinkedListAL.ListNode pHead) {
+
+    }
+
+    public LinkedListAL.ListNode FindKthToTail(LinkedListAL.ListNode head, int k) {
+
+    }
+
+    public LinkedListAL.ListNode FindFirstCommonNode2(LinkedListAL.ListNode pHead1, LinkedListAL.ListNode pHead2) {
+
+    }
+
+    /**
+     * digui
+     */
+    public LinkedListAL.ListNode ReverseList(LinkedListAL.ListNode head) {
+
+    }
+
+    public LinkedListAL.ListNode deleteDuplication2(LinkedListAL.ListNode pHead) {
+
+    }
+
+    public LinkedListAL.ListNode Merge(LinkedListAL.ListNode list1, LinkedListAL.ListNode list2) {
+
+    }
+
+
+}
---
+++ algorithm/src/interview/TreeInterAL.java
@@ -0,0 +1,87 @@
+package interview;
+
+public class TreeInterAL {
+
+    public class TreeNode {
+        int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     *
+     * 类型
+     */
+    public int TreeDepth(TreeNode root) {
+
+    }
+
+    public int isBalance(TreeNode root) {
+
+    }
+
+    public int isBinarySort(TreeNode root) {
+
+    }
+
+    TreeAL.TreeNode KthNode(TreeAL.TreeNode pRoot, int k) {
+
+    }
+
+    public boolean VerifySquenceOfBST(int [] sequence) {
+
+    }
+
+
+    /**
+     * goujian
+     */
+    public TreeAL.TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+
+    }
+
+    String Serialize(TreeAL.TreeNode root) {
+
+    }
+
+    TreeAL.TreeNode Deserialize(String str) {
+
+    }
+
+
+    /**
+     * specil strcut
+     */
+    public void Mirror(TreeAL.TreeNode root) {
+
+    }
+
+    boolean isSymmetrical(TreeAL.TreeNode pRoot) {
+
+    }
+
+    public boolean HasSubtree(TreeAL.TreeNode root1, TreeAL.TreeNode root2) {
+
+    }
+
+    /**
+     * specil op
+     */
+    private void collectAllPaths(TreeAL.TreeNode root, int target) {
+
+    }
+
+    public TreeAL.TreeNode Convert(TreeAL.TreeNode pRootOfTree) {
+
+    }
+
+
+    /**
+     * 遍历
+     */
+
+}
